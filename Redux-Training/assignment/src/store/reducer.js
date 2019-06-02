c945616ae45bece9100411e47d127cc9e3479398
Aidan <aidansoftwaredev@gmail.com>
2019-06-02 20:53:12 +0100

    Assignment

---
+++ Redux-Training/assignment/src/store/reducer.js
@@ -0,0 +1,30 @@
+import * as actions from './actions'
+
+const initialState = {
+  persons: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.ADD_PERSON:
+    const newPerson = {
+      id: Math.random(), // not really unique but good enough here!
+      name: 'Aidan',
+      age: Math.floor( Math.random() * 40 )
+    }
+
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson)
+      }
+    case actions.REMOVE_PERSON:
+      return {
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.personId)
+      }
+  }
+
+  return state;
+}
+
+export default reducer;
